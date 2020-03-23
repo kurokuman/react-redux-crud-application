89219a1b1f0efd7bf69b5a87dc56058b82f45508
kurokuman <klg.pkt0812@gmail.com>
2020-03-23 17:42:08 +0900

    create functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 
-class App extends Component {
-  render(){
+// class App extends Component {
+//   render(){
+//   return (
+//     <div>
+//       <h1 className="foo" onClick={()=>{console.log("clidk")}}>Hello World</h1>
+//     </div>
+//   );
+//   }
+// }
+
+const App = () =>{
   return (
-    <div>
-      <h1 className="foo" onClick={()=>{console.log("clidk")}}>Hello World</h1>
-    </div>
-  );
-  }
+    <div>func</div>
+  )
 }
 
 export default App;
