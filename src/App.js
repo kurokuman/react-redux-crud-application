915267d867451617f1ff5402d31e74ab17a52afe
kurokuman <klg.pkt0812@gmail.com>
2020-03-23 17:03:38 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="foo" onClick={()=>{console.log("clidk")}}>Hello World</h1>
     </div>
   );
+  }
 }
 
 export default App;
