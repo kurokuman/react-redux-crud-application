76b2efa6a7bb4d97879c46694e27dd804ff120ff
kurokuman <klg.pkt0812@gmail.com>
2020-03-30 15:58:47 +0900

    section34

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 export const READ_EVENTS = "READ_EVENTS"
+export const CREATE_EVENT = "CREATE_EVENT"
 
 const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1"
 const QUERYSTRING = "?token=token123"
@@ -10,3 +11,8 @@ export const readEvents = () => async (dispatch) => {
    dispatch({type: READ_EVENTS, responese}) 
 }
 
+export const postEvent = values => async (dispatch) => {
+  const responese = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+  console.log(responese)
+   dispatch({type: CREATE_EVENT, responese}) 
+}
